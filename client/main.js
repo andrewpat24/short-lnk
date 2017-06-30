8b75747e41b86f3dede1c6ef84e84940fd3661dd
Andrew Patterson <apat24@mail.sfsu.edu>
2017-06-29 22:41:32 -0700

    Logging out and tracking auth status

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 
 
 import Signup from '../imports/ui/Signup';
@@ -12,8 +13,10 @@ import Login from '../imports/ui/Login';
 // Create and import basic version of Login
 // Create the route for "/" and render Login
 
-window.browserHistory = browserHistory;
 
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links']
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Login}/>
@@ -22,6 +25,34 @@ const routes = (
     <Route path="*" component={NotFound}/>
   </Router>
 );
+
+
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathname = browserHistory.getCurrentLocation().pathname;
+
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  // if on unauthenticatedPage and logged in, redirect to /links
+    // browserHistory.push
+  // if on authenticated page and not logged in, redirect to '/'
+    // browserHistory.push
+  // no else
+
+  if (isAuthenticated) {
+    if(isUnauthenticatedPage){
+      browserHistory.push('/links');
+    }
+  } else {
+    if(isAuthenticatedPage){
+      browserHistory.push('/')
+    }
+  }
+
+  console.log('isAuthenticated', isAuthenticated);
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { Accounts } from 'meteor/accounts-base'
 
 export default class Link extends React.Component {
+  onLogout() {
+      Accounts.logout();
+  }
+
   render(){
     return (
       <div>
         <h1>Your Links</h1>
-        <button onClick={() => {
-          browserHistory.push('/')
-        }}>Logout</button>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
       </div>
     );
   }
